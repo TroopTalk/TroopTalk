dab552dd2ca8cb046443ae94239b27685109a08a
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-03-22 13:09:23 -0500

    optimized stories.jsx // trying poppins font for app

--- client/src/components/stories/Stories.jsx
+++ client/src/components/stories/Stories.jsx
@@ -6,15 +6,13 @@ import "./stories.scss";
 
 const Stories = () => {
   const { currentUser } = useContext(AuthContext);
-  const { toggle, darkMode } = useContext(DarkModeContext);
-  const darkLight = { color: darkMode ? "#fff" : "#000" };
-
-  const { isLoading, error, data } = useQuery(["stories"], async () => {
-    const res = await makeRequest.get("/api/stories/");
-    return res.data;
-  });
+  const darkMode = useContext(DarkModeContext);
 
   const [newStory, setNewStory] = useState("");
+  const { data, isLoading, isError } = useQuery("stories", async () => {
+    const response = await makeRequest.get("/api/stories/");
+    return response.data;
+  });
 
   const addStoryMutation = useMutation(async (newStoryData) => {
     const res = await makeRequest.post("/api/stories/", newStoryData);
@@ -22,22 +20,22 @@ const Stories = () => {
   });
 
   const handleAddStory = () => {
-    addStoryMutation.mutate({ name: newStory }); // Call the mutate function with the new story data
-    setNewStory(""); // Reset the input field after adding the new story
+    addStoryMutation.mutate({ name: newStory });
+    setNewStory("");
   };
 
   return (
     <div className="STORIES__">
       <div className="STORIES__story">
-        <img src={"/upload/" + currentUser.profilePic} alt="" />
+        <img src={`/upload/${currentUser.profilePic}`} alt="" />
         <span>{currentUser.name}</span>
         <input type="text" value={newStory} onChange={(e) => setNewStory(e.target.value)} />
         <button onClick={handleAddStory}>+</button>
       </div>
-      {error && <div style={darkLight}>Something went wrong</div>}
-      {isLoading && <div style={darkLight}>Loading...</div>}
+      {isError && <div style={{ color: darkMode ? "#fff" : "#000" }}>Something went wrong</div>}
+      {isLoading && <div style={{ color: darkMode ? "#fff" : "#000" }}>Loading...</div>}
       {!isLoading &&
-        !error &&
+        !isError &&
         data.map((story) => (
           <div className="STORIES__story" key={story.id}>
             <img src={story.img} alt="" />
