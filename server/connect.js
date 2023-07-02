87e29ce1771c5b6e43ef065262092ee180a5630e
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-07-02 15:12:17 -0500

    turned backend into  mongoDB based back end

--- server/connect.js
+++ server/connect.js
@@ -1,11 +1,11 @@
-import mysql from 'mysql';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
-export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATA,
-});
+const uri = process.env.MONGO_URI;
+
+export const connectDB = () => {
+  console.log("Connecting to mongoDB");
+  mongoose.connect(uri);
+};
--- server/server.js
+++ server/server.js
@@ -1,33 +1,30 @@
-// import { cookieParser, cors, dotenv, express } from "./packages.js";
+import { connectDB } from "./connect.js";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import Routes from "./routes/export.js";
-
 dotenv.config();
 const app = express();
 app.use(express.json());
-
-//middlewares
-app.use((req, res, next) => {
-  res.header(process.env.CORS_MSG, true);
-  cors({
-    origin: process.env.CORS_ORG,
-  })(req, res, next);
-});
-
 app.use(cookieParser());
 
-app.use(process.env.AUTH_ROUTE, Routes.authRoutes);
-app.use(process.env.COMMENT_ROUTE, Routes.commentRoutes);
-app.use(process.env.LIKE_ROUTE, Routes.likeRoutes);
-app.use(process.env.POST_ROUTE, Routes.postRoutes);
-app.use(process.env.RELATIONSHIP_ROUTE, Routes.relationshipRoutes);
-app.use(process.env.STORY_ROUTE, Routes.storyRoutes);
-app.use(process.env.USER_ROUTE, Routes.userRoutes);
+app.use(
+  cors({
+    origin: process.env.CORS_ORG,
+  }),
+);
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log("Server is UP!");
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    console.log("Connected to MongoDB successfully!");
+    app.listen(process.env.SERVER_PORT, () => {
+      console.log("Server is UP!");
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+  }
+};
+
+startServer();
