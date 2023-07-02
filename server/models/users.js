19c54cfa3a15bc2e03715f6ec5126fda53a1a3ad
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-07-02 18:23:33 -0500

    updated the user model for tthe database

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,21 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: false,
+    unique: false,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  branch: {
+    type: String,
+    required: true,
+    unique: false,
+  },
   email: {
     type: String,
     required: true,
