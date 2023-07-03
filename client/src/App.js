949b1ffc1dcde9dd8a1a2c6cf40c65d1fa558d75
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-07-02 20:43:59 -0500

    clearing stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes, Outlet, useNavigate } from "react-router-dom";
-import { ErrorBoundary } from "react-error-boundary";
 import { Friends, Home, Login, Profile, Register, Messages } from "./pages/export.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LeftBar, RightBar } from "./components/export.js";
@@ -9,15 +8,30 @@ import React, { useContext, useEffect } from "react";
 import Navbar from "./components/navbar/Navbar.jsx";
 import "./style.scss";
 
-// ErrorFallback component to display an error message or fallback UI
-const ErrorFallback = ({ error }) => {
-  return (
-    <div>
-      <h2>Something went wrong</h2>
-      <p>{error.message}</p>
-    </div>
-  );
-};
+// ErrorBoundary component to catch and handle errors
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Something went wrong</h2>
+          <p>An error occurred while rendering this page.</p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
 
 function App() {
   const { currentUser } = useContext(AuthContext);
@@ -64,10 +78,12 @@ function App() {
             path=""
             element={
               <ProtectedRoute>
-                <Layout />
+                <ErrorBoundary>
+                  <Layout />
+                </ErrorBoundary>
               </ProtectedRoute>
             }>
-            <Route path="/" element={<Home />} />
+            <Route path="/" component={Home} />
             <Route path="messages/:id" element={<Messages />} />
             <Route path="profile/:id" element={<Profile />} />
             <Route path="friends" element={<Friends />} />
--- server/connect.js
+++ server/connect.js
@@ -3,14 +3,15 @@ import mongoose from "mongoose";
 
 dotenv.config();
 
-const uri = 'mongodb+srv://root:root@trooptalk.7epqmov.mongodb.net/';
+const uri = process.env.MONGODB_URI;
 
 export const connectDB = () => {
   console.log("Connecting to MongoDB");
-  mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("Connected to MongoDB successfully!"))
     .catch((error) => console.error("Failed to connect to MongoDB:", error));
 };
