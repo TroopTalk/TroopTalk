164099bd39eb527bec3f8c768eadf008217fe88b
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-07-09 18:56:25 -0500

    users can now remove selected image for upload / styling improvments

--- client/src/components/share/ShareTop.jsx
+++ client/src/components/share/ShareTop.jsx
@@ -1,10 +1,19 @@
-const ShareTop = ({ placeholder, onChange, value, file }) => {
+import { Button } from "@mui/material";
+
+const ShareTop = ({ placeholder, onChange, value, file, onRemovePicture }) => {
   return (
     <div className="SHARE__top">
       <div className="SHARE__left">
         <input type="text" placeholder={placeholder} onChange={onChange} value={value} />
       </div>
-      <div className="SHARE__right">{file && <img className="SHARE__file" alt="Selected file preview" src={URL.createObjectURL(file)} />}</div>
+      <div className="SHARE__right">
+        {file && (
+          <div className="SHARE_right_file-preview">
+            <img className="SHARE__file" alt="Selected file preview" src={URL.createObjectURL(file)} />
+            <Button variant="contained" onClick={onRemovePicture}>Remove</Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
